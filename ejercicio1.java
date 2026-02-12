ab0b4a0f610b57463e8ad5f9fe4693beb6613a14
lambetetica69 <avillalbagarde@gmail.com>
2026-02-12 12:43:54 +0100

    prueba 2

--- ejercicio1.java
+++ ejercicio1.java
@@ -7,9 +7,7 @@ public static void multiplos (int numeroMax) {
          if (i%49==0){
             System.out.println("Multiplo de 49: " +i);
         }
-         else if(i%100==0){
-            System.out.println("Multiplo de 100: " +i);
-        }   
+          
         
     }        
   }
